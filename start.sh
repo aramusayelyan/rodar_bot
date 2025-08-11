90d0fe75ed872666adcbe164327e182447356181
Ara Musayelyan <110891675+aramusayelyan@users.noreply.github.com>
2025-08-12 00:04:53 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-set -o errexit
-# Add Chrome and ChromeDriver to PATH
-export PATH="$PATH:/opt/render/project/.render/chrome/opt/google/chrome"
-export PATH="$PATH:/opt/render/project/.render/chromedriver"
-# Start the Python bot script
+# Start the Python Telegram bot in polling mode
 python main.py
