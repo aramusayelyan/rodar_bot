62cabe1f75b9cb2f3bc008914ecd72d96e2552d0
Ara Musayelyan <110891675+aramusayelyan@users.noreply.github.com>
2025-08-11 19:10:45 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Set environment variables for Chrome binary and Chromedriver
-export GOOGLE_CHROME_BIN="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome"
-export CHROMEDRIVER_PATH="/opt/render/project/.render/chromedriver/chromedriver"
-
-# Optionally, add Chrome to PATH (not strictly required if GOOGLE_CHROME_BIN is set)
-export PATH="$PATH:/opt/render/project/.render/chrome/opt/google/chrome"
-
-# Start the Telegram bot
+# Add Chrome and Chromedriver to PATH
+export PATH="$PATH:/opt/render/project/.render/chrome/opt/google/chrome:/opt/render/project/.render/chromedriver"
+# Run the Telegram bot
 python main.py
