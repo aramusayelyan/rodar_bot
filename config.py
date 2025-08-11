a7d317f41d0df9e0be384e01cec285f7f0ca9e4d
Ara Musayelyan <110891675+aramusayelyan@users.noreply.github.com>
2025-08-11 19:29:10 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-# ⚠️ Թոքենը պահիր ENV-ում Render-ում՝ TELEGRAM_TOKEN անունով:
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 if not TELEGRAM_TOKEN:
-    raise RuntimeError("TELEGRAM_TOKEN env var is missing!")
+    raise RuntimeError("Missing TELEGRAM_TOKEN env var")
