50ee9cce83343bdc9272b5494ba6f4f9dbb1ebdc
Ara Musayelyan <110891675+aramusayelyan@users.noreply.github.com>
2025-08-11 22:48:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 import os
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# Telegram Bot Token (set this in your environment or directly assign string - but avoid committing the actual token)
+TOKEN = os.getenv("BOT_TOKEN", "8435957231:AAGcgf1KeLydzEaKJB4loMI2aXW-wfz9x6c")
